c508b43b5bc51f554498309cdfa9c41b56a8ac0e
maka70vv <25.makarovv@gmail.com>
2024-04-10 19:18:29 +0600

    tests for calculator

---
+++ buy_policy/tests/test_api.py
@@ -0,0 +1,58 @@
+from datetime import date
+
+from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework_simplejwt.tokens import AccessToken
+
+from countries.models import PriceByCountry, Country
+from exchange_rates.models import DailyExchangeRates
+from travel_agency.models import TravelAgency
+from users.models import User
+
+
+class BuyPolicyAPITests(APITestCase):
+    def setUp(self):
+        agency = TravelAgency.objects.create(name="Test Agency", inn='12345678', commission=10)
+        self.user = User.objects.create_user(username='testuser', password='testpassword', inn="12345678", travel_agency=agency)
+        self.token = self.get_token(self.user)
+        DailyExchangeRates.objects.create(eur_rate=1.2, usd_rate=1.0, rub_rate=0.014, date=date.today())
+        PriceByCountry.objects.create(country=Country.objects.create(name="Test Country"), currency='USD',
+                                      insurance_summ=1000, price_up_to_7days=100, price_up_to_15days=200,
+                                      price_up_to_30days=300, price_up_to_90days=400, price_up_to_180days=500,
+                                      price_up_to_365days=600)
+
+    def get_token(self, user):
+        access = AccessToken.for_user(user)
+        return str(access)
+
+    def test_create_buy_policy(self):
+        data = [{
+            "customer_inn": "373298287",
+            "passport_series_num": "string",
+            "sex": "1",
+            "birth_date": "1993-11-07",
+            "email": "skkgstan@gmail.com",
+            "last_name": "string",
+            "first_name": "string",
+            "patronymic": "string",
+            "phone_number": "string",
+            "phone_type": "1",
+            "start_date": "2024-09-24",
+            "end_date": "2024-09-25",
+            "purpose": "1",
+            "skiing": False,
+            "sport_activities": False,
+            "dangerous_activities": False,
+            "territory_and_currency": 1,
+            "insurance_summ": 1
+        }]
+        url = reverse('save_policy')
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.token)
+        response = self.client.post(url, data, format='json')
+
+        self.assertEqual(float(response.data[0]['price_with_taxes_kgs']), 130)
+        self.assertEqual(float(response.data[0]['price_without_taxes_kgs']), 100)
+        self.assertEqual(float(response.data[0]['commission_summ']), 10)
+        self.assertEqual(float(response.data[0]['profit_summ']), 90)
+        self.assertEqual(float(response.data[0]['price_exchange']), 130)
+        self.assertEqual(float(response.data[0]['taxes_summ']), 30)
---
+++ buy_policy/tests/test_services.py
@@ -0,0 +1,82 @@
+from datetime import date
+from django.test import TestCase
+from exchange_rates.models import DailyExchangeRates
+from countries.models import PriceByCountry, Country
+from buy_policy.services import (
+    calculate_age,
+    calculate_coefficient,
+    calculate_price,
+    convert_to_kgs,
+    calculate_taxes,
+    calculate_price_with_taxes,
+    calculate_commission_summ,
+    calculate_profit,
+    save_insurance_price,
+    calculate_insurance_price
+)
+
+
+class TestUtilsFunctions(TestCase):
+    def setUp(self):
+        self.birth_date = date(1990, 1, 1)
+        self.start_date = date(2024, 4, 1)
+        self.end_date = date(2024, 4, 10)
+        self.insurance_summ = PriceByCountry(country=Country.objects.create(name="Test Country"), currency='USD',
+                                             insurance_summ=1000, price_up_to_7days=100, price_up_to_15days=200,
+                                             price_up_to_30days=300, price_up_to_90days=400, price_up_to_180days=500,
+                                             price_up_to_365days=600)
+        self.exchange_rates = DailyExchangeRates(eur_rate=1.2, usd_rate=1.0, rub_rate=0.014)
+        self.insured = 1
+        self.commission = 10
+
+    def test_calculate_age(self):
+        age = calculate_age(self.birth_date)
+        self.assertEqual(age, 34)  # Проверяем, что возраст вычисляется корректно
+
+    def test_calculate_coefficient(self):
+        coefficient = calculate_coefficient(30, skiing=True, sport_activities=False, dangerous_activities=False,
+                                            insured=5)
+        self.assertEqual(coefficient, 2)  # Проверяем, что коэффициент вычисляется корректно
+
+    def test_calculate_price(self):
+        price = calculate_price(5, 2, self.insurance_summ)
+        self.assertEqual(price, 1000)  # Проверяем, что цена вычисляется корректно
+
+    def test_convert_to_kgs(self):
+        price_kgs = convert_to_kgs(100, self.insurance_summ, self.exchange_rates)
+        self.assertEqual(price_kgs, 100)  # Проверяем, что конвертация в KGS работает корректно
+
+    def test_calculate_taxes(self):
+        taxes = calculate_taxes(100)
+        self.assertEqual(taxes, 30)  # Проверяем, что налоги вычисляются корректно
+
+    def test_calculate_price_with_taxes(self):
+        price_with_taxes = calculate_price_with_taxes(100, 30)
+        self.assertEqual(price_with_taxes, 130)  # Проверяем, что цена с налогами вычисляется корректно
+
+    def test_calculate_commission_summ(self):
+        commission_summ = calculate_commission_summ(100, self.commission)
+        self.assertEqual(commission_summ, 10)  # Проверяем, что комиссия вычисляется корректно
+
+    def test_calculate_profit(self):
+        profit = calculate_profit(100, 10)
+        self.assertEqual(profit, 90)  # Проверяем, что прибыль вычисляется корректно
+
+    def test_save_insurance_price(self):
+        prices = save_insurance_price(self.birth_date, skiing=True, sport_activities=False, dangerous_activities=False,
+                                      start_date=self.start_date, end_date=self.end_date,
+                                      insurance_summ=self.insurance_summ,
+                                      exchange_rates=self.exchange_rates, insured=self.insured,
+                                      commission=self.commission)
+        self.assertEqual(prices['age'], 34)  # Проверяем, что возраст сохраняется корректно
+        self.assertEqual(prices['price_kgs'], 4680)  # Проверяем, что цена в KGS сохраняется корректно
+        self.assertEqual(prices['price_exchange'], 4680)  # Проверяем, что цена в валюте сохраняется корректно
+
+    def test_calculate_insurance_price(self):
+        prices = calculate_insurance_price(self.birth_date, skiing=True, sport_activities=False,
+                                           dangerous_activities=False,
+                                           start_date=self.start_date, end_date=self.end_date,
+                                           insurance_summ=self.insurance_summ,
+                                           exchange_rates=self.exchange_rates, insured=self.insured)
+        self.assertEqual(prices['price_kgs'], 4680)  # Проверяем, что цена в KGS вычисляется корректно
+        self.assertEqual(prices['price_exchange'], 4680)  # Проверяем, что цена в валюте вычисляется корректно
